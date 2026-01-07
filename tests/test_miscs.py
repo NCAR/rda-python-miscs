c70f322533d058b768ce3be2bc29552d50c532c2
zaihuaji <zaihuaji@gmail.com>
2026-01-07 15:15:11 -0600

    save

--- tests/test_miscs.py
+++ tests/test_miscs.py
@@ -3,14 +3,14 @@
 import pytest
 
 def test_miscs():
-   import rda_python_mics.bashqsub
-   import rda_python_mics.tcshqsub
-   import rda_python_mics.gdexls
-   import rda_python_mics.pgwget
-   import rda_python_mics.rdacp
-   import rda_python_mics.rdkill
-   import rda_python_mics.rdamod
-   import rda_python_mics.rdaown
-   import rda_python_mics.rdaps
-   import rda_python_mics.rdasub
-   import rda_python_mics.rdazip
+   import rda_python_miscs.bashqsub
+   import rda_python_miscs.tcshqsub
+   import rda_python_miscs.gdexls
+   import rda_python_miscs.pgwget
+   import rda_python_miscs.rdacp
+   import rda_python_miscs.rdkill
+   import rda_python_miscs.rdamod
+   import rda_python_miscs.rdaown
+   import rda_python_miscs.rdaps
+   import rda_python_miscs.rdasub
+   import rda_python_miscs.rdazip
